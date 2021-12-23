dfe8ea593de02e412ba9cf1f4d601f7451634b6e
hann0t <hansolivas1@gmail.com>
2021-12-23 08:47:55 -0500

    files moved.

--- src/CurrencyContainer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './CurrencyContainer.css';
-
-const CurrencyContainer = ({ children }) => {
-  return <div class='currency-container'>{children}</div>;
-};
-
-export { CurrencyContainer };
