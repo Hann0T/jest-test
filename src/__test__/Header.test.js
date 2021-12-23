941b0bd0b0d01154b442a8f3af5928bb15d7f6a8
hann0t <hansolivas1@gmail.com>
2021-12-23 08:49:32 -0500

    new test.

--- src/__test__/Header.test.js
+++ src/__test__/Header.test.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow, mount } from 'enzyme';
 
-import { Header } from '../Header';
+import { Header } from '../Components/Header';
 
-test("The header component didn't change", () => {
-  const header = renderer.create(<Header />);
+describe('<Header />', () => {
+  test('render header component', () => {
+    const header = mount(<Header />);
+    expect(header.length).toEqual(1);
+  });
 
-  expect(header.toJSON()).toMatchSnapshot();
+  test("The header component didn't change", () => {
+    const header = renderer.create(<Header />);
+
+    expect(header.toJSON()).toMatchSnapshot();
+  });
+
+  test("The header should have 'Convert US Dollars to Peruvian Soles' in the title", () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('h1').text()).toMatch(
+      /Convert US Dollars to Peruvian Soles/,
+    );
+  });
 });
